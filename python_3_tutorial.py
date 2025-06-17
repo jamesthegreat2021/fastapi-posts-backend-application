da54665a01c3baa6b70171b932220395786f2ed3
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-06-17 14:06:30 +0300

    added sample python folder for testing some python codes

---
+++ python_3_tutorial.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+#dealing with lists
+
+names_list1 = ['james', 'julius', 'rodrick']
+names_list2 = ['anna', 'adelah', 'barton']
+names_list3 = ['steven', 'paul', 'derrick']
+
+names_list4 = [names_list1, 'james', 'the', 'great', 'is', 'ai', 'engineer']
+print(names_list4)
+
+names_list5 = names_list1[0]
+print(names_list5)
